3e703139f41195d382901c204cf68063d03eacf2
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2023-10-21 19:23:59 +0700

    - add class img

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 const Card = ({ image, name, age }) => {
   return (
-    <li>
-      <img src={image} alt={name} width="100px" />
-      <h4>{name}</h4>
-      <h4>{age} years</h4>
-    </li>
+    <article className="person">
+      <img src={image} alt={name} className="img" />
+      <div>
+        <h4>{name}</h4>
+        <h4>{age} years</h4>
+      </div>
+    </article>
   )
 }
 
