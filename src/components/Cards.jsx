0fa7d3b5a2e9e0bc8ad77b2ca2364db9782098af
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2023-10-21 19:24:18 +0700

    - add class css + fix bug user length

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,23 +6,25 @@ const Cards = () => {
   const [users, setUsers] = useState(datas)
 
   return (
-    <section>
-      <h2>{datas.length} BirthDay Today</h2>
-      <ul>
-        {users.map((data) => {
-          return <Card key={data.id} {...data} />
-        })}
-      </ul>
-      <button
-        type="button"
-        className="btn"
-        onClick={() => {
-          setUsers([])
-        }}
-      >
-        Clear All
-      </button>
-    </section>
+    <main>
+      <section className="container">
+        <h2>{users.length} BirthDay Today</h2>
+        <ul>
+          {users.map((data) => {
+            return <Card key={data.id} {...data} />
+          })}
+        </ul>
+        <button
+          type="button"
+          className="btn btn-block"
+          onClick={() => {
+            setUsers([])
+          }}
+        >
+          Clear All
+        </button>
+      </section>
+    </main>
   )
 }
 
